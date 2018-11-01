3d9c377a33ebf77f8e7ad47aa7adaa56a7d82d0d
Yurii Ivanov <mrz.ivanov@gmail.com>
2018-11-01 15:38:58 +0200

    init

---
+++ buildImage.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker stop typfrontend
+docker image rm typfrontend
+
+docker build -t typfrontend .
+docker run --rm --name typfrontend -p 3000:4200 -v %cd%/src:/usr/typ/src typfrontend
