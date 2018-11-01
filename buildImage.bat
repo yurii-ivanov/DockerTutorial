6c276ec96e2d06f5a200bbc8902bee17fa3bbbd1
Yurii Ivanov <mrz.ivanov@gmail.com>
2018-11-01 15:50:22 +0200

    NEw build file

--- buildImage.bat
+++ buildImage.bat
@@ -2,5 +2,7 @@
 docker stop typfrontend
 docker image rm typfrontend
 
+alias %cd%=$(pwd)
+
 docker build -t typfrontend .
 docker run --rm --name typfrontend -p 3000:4200 -v %cd%/src:/usr/typ/src typfrontend
